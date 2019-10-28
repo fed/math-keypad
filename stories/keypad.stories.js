e8bf8aab62918a7e63112b6d93fbabef5d1b1e10
Federico Knüssel <fknussel@gmail.com>
2019-10-28 19:21:52 +0100

    Set up Storybook

---
+++ stories/keypad.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MathKeypad from '../src';
+
+export default {
+  title: 'Math Keypad'
+};
+
+// Basic keypad
+export const basic = () => <MathKeypad />;
+
+basic.story = {
+  name: 'Basic'
+};
